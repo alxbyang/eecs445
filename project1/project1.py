dd613d66998f34b448be77cf05fbcb72b33c7f1e
Alex Yang <alxbyang@gmail.com>
2025-02-06 02:39:53 -0500

    p1 first commit

---
+++ project1/project1.py
@@ -0,0 +1,328 @@
+"""
+EECS 445 Winter 2025
+
+This script should contain most of the work for the project. You will need to fill in every TODO comment.
+"""
+
+
+import numpy as np
+import numpy.typing as npt
+import pandas as pd
+import yaml
+from matplotlib import pyplot as plt
+from sklearn import metrics
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import MinMaxScaler
+
+import helper
+
+
+__all__ = [
+    "generate_feature_vector",
+    "impute_missing_values",
+    "normalize_feature_matrix",
+    "get_classifier",
+    "performance",
+    "cv_performance",
+    "select_param_logreg",
+    "select_param_RBF",
+    "plot_weight",
+]
+
+
+# load configuration for the project, specifying the random seed and variable types
+with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+seed = config["seed"]
+np.random.seed(seed)
+
+
+def generate_feature_vector(df: pd.DataFrame) -> dict[str, float]:
+    """
+    Reads a dataframe containing all measurements for a single patient
+    within the first 48 hours of the ICU admission, and convert it into
+    a feature vector.
+
+    Args:
+        df: pd.Dataframe, with columns [Time, Variable, Value]
+
+    Returns:
+        a python dictionary of format {feature_name: feature_value}
+        for example, {"Age": 32, "Gender": 0, "max_HR": 84, ...}
+    """
+    static_variables = config["static"]
+    timeseries_variables = config["timeseries"]
+    
+    # TODO: 1) Replace unknown values with np.nan
+    # NOTE: pd.DataFrame.replace() may be helpful here, refer to documentation for details
+    df_replaced = None
+
+    # Extract time-invariant and time-varying features (look into documentation for pd.DataFrame.iloc)
+    static, timeseries = df.iloc[0:5], df.iloc[5:]
+
+    feature_dict = {}
+    # TODO: 2) extract raw values of time-invariant variables into feature dict
+
+    # TODO  3) extract max of time-varying variables into feature dict
+    
+    return feature_dict
+
+
+def impute_missing_values(X: npt.NDArray) -> npt.NDArray:
+    """
+    For each feature column, impute missing values (np.nan) with the population mean for that feature.
+
+    Args:
+        X: array of shape (N, d) which could contain missing values
+        
+    Returns:
+        X: array of shape (N, d) without missing values
+    """
+    pass  # TODO: implement
+
+
+def normalize_feature_matrix(X: npt.NDArray) -> npt.NDArray:
+    """
+    For each feature column, normalize all values to range [0, 1].
+
+    Args:
+        X: array of shape (N, d).
+
+    Returns:
+        X: array of shape (N, d). Values are normalized per column.
+    """
+    # NOTE: sklearn.preprocessing.MinMaxScaler may be helpful
+    pass  # TODO: implement
+
+
+def get_classifier(
+    loss: str = "logistic",
+    penalty: str | None = None,
+    C: float = 1.0,
+    class_weight: dict[int, float] | None = None,
+    kernel: str = "rbf",
+    gamma: float = 0.1,
+) -> KernelRidge | LogisticRegression:
+    """
+    Return a classifier based on the given loss, penalty function and regularization parameter C.
+
+    Args:
+        loss: Specifies the loss function to use.
+        penalty: The type of penalty for regularization.
+        C: Regularization strength parameter.
+        class_weight: Weights associated with classes.
+        kernel : Kernel type to be used in Kernel Ridge Regression.
+        gamma: Kernel coefficient.
+
+    Returns:
+        A classifier based on the specified arguments.
+    """
+    # TODO (optional, but highly recommended): implement function based on docstring
+
+    if loss == "logistic":
+        pass
+    elif loss == "squared_error":
+        pass
+
+
+def performance(
+    clf_trained: KernelRidge | LogisticRegression,
+    X: npt.NDArray,
+    y_true: npt.NDArray,
+    metric: str = "accuracy"
+) -> float:
+    """
+    Calculates the performance metric as evaluated on the true labels
+    y_true versus the predicted scores from clf_trained and X.
+    Returns single sample performance as specified by the user. Note: you may
+    want to implement an additional helper function to reduce code redundancy.
+
+    Args:
+        clf_trained: a fitted instance of sklearn estimator
+        X : (n,d) np.array containing features
+        y_true: (n,) np.array containing true labels
+        metric: string specifying the performance metric (default='accuracy'
+                other options: 'precision', 'f1-score', 'auroc', 'average_precision',
+                'sensitivity', and 'specificity')
+    Returns:
+        peformance for the specific metric
+    """
+    # This is an optional but very useful function to implement.
+    # See the sklearn.metrics documentation for pointers on how to implement
+    # the requested metrics.
+    pass  # TODO: implement
+
+
+def cv_performance(
+    clf: KernelRidge | LogisticRegression,
+    X: npt.NDArray,
+    y: npt.NDArray,
+    metric: str = "accuracy",
+    k: int = 5,
+) -> tuple[float, float, float]:
+    """
+    Splits the data X and the labels y into k-folds and runs k-fold
+    cross-validation: for each fold i in 1...k, trains a classifier on
+    all the data except the ith fold, and tests on the ith fold.
+    Calculates the k-fold cross-validation performance metric for classifier
+    clf by averaging the performance across folds.
+
+    Args:
+        clf: an instance of a sklearn classifier
+        X: (n,d) array of feature vectors, where n is the number of examples
+           and d is the number of features
+        y: (n,) vector of binary labels {1,-1}
+        k: the number of folds (default=5)
+        metric: the performance metric (default='accuracy'
+             other options: 'precision', 'f1-score', 'auroc', 'average_precision',
+             'sensitivity', and 'specificity')
+
+    Returns:
+        a tuple containing (mean, min, max) cross-validation performance across the k folds
+    """
+    # NOTE: you may find sklearn.model_selection.StratifiedKFold helpful
+    pass  # TODO: implement
+
+
+def select_param_logreg(
+    X: npt.NDArray,
+    y: npt.NDArray,
+    metric: str = "accuracy",
+    k: int = 5,
+    C_range: list[float] = [],
+    penalties: list[str] = ["l2", "l1"],
+) -> tuple[float, str]:
+    """
+    Sweeps different settings for the hyperparameter of a logistic regression, calculating the k-fold CV
+    performance for each setting on X, y.
+
+    Args:
+        X: (n,d) array of feature vectors, where n is the number of examples
+        and d is the number of features
+        y: (n,) array of binary labels {1,-1}
+        k: int specifying the number of folds (default=5)
+        metric: string specifying the performance metric for which to optimize (default='accuracy',
+             other options: 'precision', 'f1-score', 'auroc', 'average_precision', 'sensitivity',
+             and 'specificity')
+        C_range: an array with C values to be searched over
+        penalties: a list of strings specifying the type of regularization penalties to be searched over
+
+    Returns:
+        The hyperparameters for a logistic regression model that maximizes the
+        average k-fold CV performance.
+    """
+    # NOTE: use your cv_performance function to evaluate the performance of each classifier
+    pass  # TODO: implement
+
+
+def select_param_RBF(
+    X: npt.NDArray[np.float64],
+    y: npt.NDArray[np.int64],
+    metric: str = "accuracy",
+    k: int = 5,
+    C_range: list[float] = [],
+    gamma_range: list[float] = [],
+) -> tuple[float, float]:
+    """
+    Sweeps different settings for the hyperparameter of a RBF Kernel Ridge Regression,
+    calculating the k-fold CV performance for each setting on X, y.
+
+    Args:
+        X: (n,d) array of feature vectors, where n is the number of examples
+        and d is the number of features
+        y: (n,) array of binary labels {1,-1}
+        k: the number of folds (default=5)
+        metric: the performance metric (default='accuracy',
+             other options: 'precision', 'f1-score', 'auroc', 'average_precision',
+             'sensitivity', and 'specificity')
+        C_range: an array with C values to be searched over
+        gamma_range: an array with gamma values to be searched over
+
+    Returns:
+        The parameter values for a RBF Kernel Ridge Regression that maximizes the
+        average k-fold CV performance.
+    """
+    # NOTE: this function should be similar to your implementation of select_param_logreg
+    pass  # TODO: implement
+
+
+def plot_weight(
+    X: npt.NDArray,
+    y: npt.NDArray,
+    C_range: list[float],
+    penalties: list[str],
+) -> None:
+    """
+    The funcion takes training data X and labels y, plots the L0-norm
+    (number of nonzero elements) of the coefficients learned by a classifier
+    as a function of the C-values of the classifier, and saves the plot.
+    Args:
+        X: (n,d) array of feature vectors, where n is the number of examples
+        and d is the number of features
+        y: (n,) array of binary labels {1,-1}
+
+    Returns:
+        None
+    """
+
+    print("Plotting the number of nonzero entries of the parameter vector as a function of C")
+
+    for penalty in penalties:
+        norm0 = []
+        for C in C_range:
+            # TODO: initialize clf with C and penalty
+            clf = None
+            
+            # TODO: fit clf to X and y
+            
+            # TODO: extract learned coefficients from clf into w
+            # NOTE: the sklearn.linear_model.LogisticRegression documentation will be helpful here
+            w = None
+            
+            # TODO: count the number of nonzero coefficients and append the count to norm0
+            non_zero_count = None
+            norm0.append(non_zero_count)
+
+        # This code will plot your L0-norm as a function of C
+        plt.plot(C_range, norm0)
+        plt.xscale("log")
+    plt.legend([penalties[0], penalties[1]])
+    plt.xlabel("Value of C")
+    plt.ylabel("Norm of theta")
+
+    plt.savefig("L0_Norm.png", dpi=200)
+    plt.close()
+
+
+def main():
+    print(f"Using Seed = {seed}")
+    # NOTE: READING IN THE DATA WILL NOT WORK UNTIL YOU HAVE FINISHED IMPLEMENTING generate_feature_vector,
+    #       fill_missing_values AND normalize_feature_matrix!
+    # NOTE: Only set debug=True when testing your implementation against debug.txt. DO NOT USE debug=True when
+    #       answering the project questions! It only loads a small sample (n = 100) of the data in debug mode,
+    #       so your performance will be very bad when working with the debug data.
+    X_train, y_train, X_test, y_test, feature_names = helper.get_project_data(debug=False)
+
+    metric_list = [
+        "accuracy",
+        "precision",
+        "f1_score",
+        "auroc",
+        "average_precision",
+        "sensitivity",
+        "specificity",
+    ]
+
+    # TODO: Questions 1, 2, 3, 4
+    # NOTE: It is highly recomended that you create functions for each
+    #       sub-question/question to organize your code!
+
+    # TODO: Question 5: Apply a classifier to heldout features, and then use
+    #       helper.save_challenge_predictions to save your predicted labels
+    # X_challenge, y_challenge, X_heldout, feature_names = helper.get_challenge_data()
+
+
+if __name__ == "__main__":
+    main()
